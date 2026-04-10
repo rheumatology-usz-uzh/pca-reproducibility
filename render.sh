98245380d32c3844b8cacd00d84a1d16ce5353fa
Christof Seiler <christof.seiler@usz.ch>
2026-04-10 13:53:29 +0200

    add dataset integration

--- render.sh
+++ render.sh
@@ -2,14 +2,16 @@
 
 module load rstudio
 
+echo "Render script: " $1
+
 # amd
 module load amd
 srun --pty -n 1 -c 2 --time=01:00:00 --mem=7G \
   apptainer exec /apps/u24/opt/containers/rstudio/4.5.2.sif \
-  quarto render seurat.qmd --output "amd.html"
+  quarto render $1 --output "amd.html"
 
 # intel
 module load intel
 srun --pty -n 1 -c 2 --time=01:00:00 --mem=7G \
   apptainer exec /apps/u24/opt/containers/rstudio/4.5.2.sif \
-  quarto render seurat.qmd --output "intel.html"
+  quarto render $1 --output "intel.html"
