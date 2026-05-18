7d332074b1180d682cfe0934963415cf5b53a05c
Christof Seiler <christof.seiler@usz.ch>
2026-05-18 18:30:26 +0200

    add mean error anlaysis

--- seurat_render.sh
+++ seurat_render.sh
@@ -24,3 +24,9 @@ for CPU in ${CPU_TYPES[@]}; do
     done
 
 done
+
+echo "comparison"
+srun --pty -n 1 -c 2 --time=01:00:00 --mem=7G \
+  Rscript -e "quarto::quarto_render( \
+    input = 'seurat_comparison.qmd') \
+  )"
