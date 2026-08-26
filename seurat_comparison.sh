2dde598b1a6e517eedf7af6ebd4a8cc1a638d9b6
Christof Seiler <christof.seiler@usz.ch>
2026-08-26 08:21:41 +0200

    add netlib vs open libs input

--- seurat_comparison.sh
+++ seurat_comparison.sh
@@ -3,8 +3,16 @@
 module load apptainer
 module load rstudio
 
-BLAS="/usr/lib/x86_64-linux-gnu/blas/libblas.so"
-LAPACK="/usr/lib/x86_64-linux-gnu/lapack/liblapack.so"
+if [ "$1" = "netlib" ]; then
+    BLAS="/usr/lib/x86_64-linux-gnu/blas/libblas.so"
+    LAPACK="/usr/lib/x86_64-linux-gnu/lapack/liblapack.so"
+else
+    BLAS="/usr/lib/x86_64-linux-gnu/openblas-pthread/libblas.so"
+    LAPACK="/usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblas.so"
+fi
+
+echo "BLAS=$BLAS"
+echo "LAPACK=$LAPACK"
 
 CPU_TYPES=("amd" "intel")
 PCA_ALGOS=("exact" "irlba" "rsvd")
@@ -28,6 +36,6 @@ done
 
 echo "comparison"
 srun --pty -n 1 -c 2 --time=01:00:00 --mem=7G \
-  apptainer exec /apps/u24/opt/containers/rstudio/4.5.2.sif \
+  apptainer exec --env LD_PRELOAD=$BLAS:$LAPACK \
+  /apps/u24/opt/containers/rstudio/4.5.2.sif \
   quarto render seurat_comparison.qmd
-
